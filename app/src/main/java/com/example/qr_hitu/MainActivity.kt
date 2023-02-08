6040fc2242cd3b6a13a55eff5315e3d92f80fa87
Wonchy <david_c_fernandes@outlook.pt>
2023-02-08 00:13:49 +0000

    QrCodeGenerator - No Download Option
    Criador de códigos QR sem opção de download

--- app/src/main/java/com/example/qr_hitu/MainActivity.kt
+++ app/src/main/java/com/example/qr_hitu/MainActivity.kt
@@ -3,41 +3,73 @@ package com.example.qr_hitu
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Button
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.qr_hitu.ui.theme.QRHITUTheme
+import com.google.zxing.BarcodeFormat
+import com.journeyapps.barcodescanner.BarcodeEncoder
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             QRHITUTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                CreateQrCode()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun CreateQrCode(modifier: Modifier = Modifier) {
+    var content by remember { mutableStateOf("") }
+    Column(Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+        if (content.isNotEmpty()){
+            val barcodeEncoder = BarcodeEncoder()
+            val bitmap = barcodeEncoder.encodeBitmap(content, BarcodeFormat.QR_CODE, 512, 512)
+            Image(
+                bitmap = bitmap.asImageBitmap(),
+                contentDescription = "QrCode"
+            )
+        }
+
+        Spacer(modifier = Modifier.height(20.dp))
+        OutlinedTextField(
+            value = content,
+            onValueChange = {
+                content = it
+            },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+            label = { Text(text = "Escreva texto para conversão", color = Color(0xFFBB86FC)) },
+            placeholder = { Text(text = "Texto a converter", color = Color.LightGray) },
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                unfocusedBorderColor = Color(0xFFBB86FC))
+
+            )
+    }
 }
 
-@Preview(showSystemUi = true)
 @Composable
-fun DefaultPreview() {
-    QRHITUTheme {
-        Greeting("Android")
-    }
+fun GenerateQrCode(content: Any) {
+
+    val barcodeEncoder = BarcodeEncoder()
+    val bitmap = barcodeEncoder.encodeBitmap(content.toString(), BarcodeFormat.QR_CODE, 512, 512)
+    Image(
+        bitmap = bitmap.asImageBitmap(),
+        contentDescription = "QrCode"
+    )
 }
