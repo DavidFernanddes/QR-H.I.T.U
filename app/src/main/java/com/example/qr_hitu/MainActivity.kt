4707508eb3e72fd163878e3d7ddd5e242f516ebd
David Fernandes <david_c_fernandes@outlook.pt>
2023-02-09 09:43:13 +0000

    Better master
    Update em código desatualizado

--- app/src/main/java/com/example/qr_hitu/MainActivity.kt
+++ app/src/main/java/com/example/qr_hitu/MainActivity.kt
@@ -3,13 +3,7 @@ package com.example.qr_hitu
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.qr_hitu.ui.theme.QRHITUTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,26 +12,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             QRHITUTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+                Surface()
+                {
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showSystemUi = true)
-@Composable
-fun DefaultPreview() {
-    QRHITUTheme {
-        Greeting("Android")
     }
 }
