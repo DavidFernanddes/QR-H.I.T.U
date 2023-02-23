2d19a1106e69ae9826cf843b55b0bb3d404a6cbd
Alexandre Caneca <alex.caneca@gmail.com>
2023-02-23 11:31:35 +0000

    Code update
    Colocar tudo em funções/procedimentos
    
    Retirar código inútil
    
    Comentar o código todo

--- app/src/main/java/com/example/qr_hitu/DataManipulation.kt
+++ app/src/main/java/com/example/qr_hitu/DataManipulation.kt
@@ -1,34 +1,83 @@
 package com.example.qr_hitu
 
+import android.util.Log
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.tasks.await
 
-val db = Firebase.firestore
+val db = Firebase.firestore //  Variável que chama o serviço da firestore
 
-fun AddDispositivo (
-    bloco : String,
+//Procedimento para adicionar novo dispositivo
+fun addDispositivo (
+    bloco : String, //  As variáveis bloco, sala e ident são o caminho para o documento na firestore
     sala : String,
     ident : String,
-    mapa : HashMap<String, String>
+    mapa : HashMap<String, String>  //  A variável mapa passa as informações a inserir
 ) {
+
+    //  .collection() identifica a coleção, .document() identifica o documento
+    //  .set() define a informação do documento atual
     db.collection("Inventário")
-        .document("$bloco")
-        .collection("$sala")
-        .document("$ident")
+        .document(bloco)
+        .collection(sala)
+        .document(ident)
         .set(mapa)
+
 }
 
-
-fun DelDispositivo (
-    bloco : String,
+//Procedimento para apagar dispositivo
+fun delDispositivo (
+    bloco : String, //  As variáveis bloco, sala e ident são o caminho para o documento na firestore
     sala : String,
     ident : String
 ) {
+
+    //  .collection() identifica a coleção, .document() identifica o documento
+    //  .delete() apaga o documento atual
     db.collection("Inventário")
-        .document("$bloco")
-        .collection("$sala")
-        .document("$ident")
+        .document(bloco)
+        .collection(sala)
+        .document(ident)
         .delete()
+
 }
 
+//Função para buscar informação na firestore e guardar num mapa
+@Composable
+fun seeDispositivo (
+    bloco : String, //  As variáveis bloco, sala e ident são o caminho para o documento na firestore
+    sala : String,
+    ident : String
+) : Map<String, Any> {
 
+    //  A variável collectionReference foi criada para não deixar o código muito denso
+    //  .collection() identifica a coleção, .document() identifica o documento
+    val collectionReference = db.collection("Inventário")
+        .document(bloco)
+        .collection(sala)
+        .document(ident)
+
+    //  Variáveis para guardar os valores que vêm da firestore
+    val (data, setData) = remember { mutableStateOf(mapOf<String, Any>()) }
+
+    //  Abre coroutine
+    LaunchedEffect(collectionReference) {
+        try {
+            //  .get() consegue o documentSnapshot, .await() espera que se recupere o documentSnapshot
+            val documentSnapshot = collectionReference.get().await()
+            //  Define o valor de setData para os valores no documentSnapshot, se for nulo define para nulo
+            setData(documentSnapshot.data ?: mapOf())
+        } catch (e: Exception) {
+            //  Log em caso de erro
+            Log.e("Firestore", "Error retrieving data", e)
+        }
+    }
+
+    //  Retorna a data
+    return data
+
+}
--- app/src/main/java/com/example/qr_hitu/MainActivity.kt
+++ app/src/main/java/com/example/qr_hitu/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.example.qr_hitu
 
-import android.content.ContentValues.TAG
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
@@ -11,10 +9,6 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.qr_hitu.ui.theme.QRHITUTheme
-import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
-import kotlinx.coroutines.tasks.await
 
 
 class MainActivity : ComponentActivity() {
@@ -31,64 +25,34 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun DB () {
-    val db = Firebase.firestore
 
+    val bloco = "Bloco E"
+    val sala = "Sala E0.05"
+    val ident = "Computador 1"
     var pc = hashMapOf(
         "Nome" to "Teste",
         "Processador" to "Intel",
         "Fonte" to "Cabo"
     )
 
-    val collectionReference = db.collection("Inventário")
-        .document("Bloco E")
-        .collection("Sala E0.05")
-
-    val (data, setData) = remember { mutableStateOf(mapOf<String, Any>()) }
-
-    LaunchedEffect(collectionReference) {
-        try {
-            val documentSnapshot = collectionReference.document("Computador").get().await()
-            setData(documentSnapshot.data ?: mapOf())
-        } catch (e: Exception) {
-            Log.e("Firestore", "Error retrieving data", e)
-        }
-    }
-
-    var mostra = false
 
     Column {
-        Button(onClick = { mostra = true }) {
-            Text("Read")
+
+        seeDispositivo(bloco, sala, ident).forEach { (key, value) ->
+            Text("$key: $value")
         }
-        if (mostra) {
-            data.forEach { (key, value) ->
-                Text("$key: $value")
-            }
+
+        Button(onClick = { addDispositivo(bloco, sala, ident, pc) }) {
+            Text("Add")
         }
-        Add(pc)
-        Del(db)
+
+        Button(onClick = { delDispositivo(bloco, sala, ident) }) {
+            Text("Del")
+        }
+
     }
 
 }
-
-@Composable
-fun Add (pc : HashMap<String, String>) {
-    Button(onClick = {
-        AddDispositivo(bloco = "Bloco", sala = "Sala E0.05", ident = "ads", pc)
-    }) {
-        Text(text = "Add")
-    }
-}
-
-@Composable
-fun Del (db : FirebaseFirestore) {
-    Button(onClick = {
-        DelDispositivo(bloco = "Bloco", sala = "Sala E0.05", ident = "ads")
-    }) {
-        Text(text = "Delete")
-    }
-}
-
 
 @Preview(showSystemUi = true)
 @Composable
