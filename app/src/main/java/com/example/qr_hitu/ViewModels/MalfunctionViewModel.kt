4209ca19d5a49454d7d859fab434cc8f21b8af67
Wonchy <david_c_fernandes@outlook.pt>
2023-05-26 15:39:54 +0100

    View model Starter

---
+++ app/src/main/java/com/example/qr_hitu/ViewModels/MalfunctionViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.qr_hitu.viewModels
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+
+class MalfunctionViewModel: ViewModel() {
+}
